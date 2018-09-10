8597522857baff41efe9a0c4fe3042912966ebef
Edwin de Jonge <edwindjonge@gmail.com>
2018-09-10 15:16:49 +0200

    updating the service

--- R/params.R
+++ R/params.R
@@ -10,7 +10,7 @@ as_params <- function(..., .list = NULL){
   .list[names(l)] <- l
   keys <- names(.list)
   keys <- sapply(keys, utils::URLencode, USE.NAMES = FALSE)
-  values <- as.character(unname(.list))
+  values <- enc2utf8(as.character(unname(.list)))
   values <- sapply(values, utils::URLencode, USE.NAMES = FALSE)
   paste0(keys,"=",values, collapse = "&")
 }
