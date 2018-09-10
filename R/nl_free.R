8181d606067687e0fff672ee0667d36ff017cfc7
Edwin de Jonge <edwindjonge@gmail.com>
2018-09-10 19:07:27 +0200

    wip

--- R/nl_free.R
+++ R/nl_free.R
@@ -5,6 +5,8 @@
 #' @param q query to geocoding service.
 #' @param ... parameters passed to the pdok webservice
 #' @param type restrict the results on a specific type.
+#' @param fl fields to return.
+#' @param df field that should be search in.
 #' @param verbose \code{logical} should the function print out messages.
 #' @inheritParams query
 #' @seealso \code{\link{nl_geocode}}
@@ -15,12 +17,14 @@ nl_free <- function( q
                    , lat     = NULL
                    , lon     = NULL
                    , type    = NULL
+                   , fl      = NULL
+                   , df      = NULL
                    , ...
                    , verbose = FALSE
                    ){
   l <- list(...)
   l <- type_filter(l, type)
-  params <- as_params(q = q, .list = l, start = start, fq = fq, lat= lat, lon = lon)
+  params <- as_params(q = q, .list = l, start = start, fq = fq, df = df, fl = fl, lat = lat, lon = lon)
   q_url <- paste0(file.path(API, "free"), "?", params)
   if (verbose){
     message(q_url)
