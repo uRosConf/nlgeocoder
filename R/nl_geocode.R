fc8d3898d4ca1402681726dffaa34342f6f23331
Edwin de Jonge <edwindjonge@gmail.com>
2018-09-10 10:39:10 +0200

    adding info

---
+++ R/nl_geocode.R
@@ -0,0 +1,7 @@
+#' geocode function
+#'
+#' Which is more user friendly than the barebones webservices, uses the same function signature as ggmap geocode
+#' @export
+nl_geocode <- function(...){
+  stop("Not working not now.")
+}
