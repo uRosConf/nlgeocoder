b0d7c34bb4ee80759b88775ffe9cf52362e272bf
Edwin de Jonge <edwindjonge@gmail.com>
2018-09-10 15:07:24 +0200

    updating documentation

--- R/nl_geocode.R
+++ R/nl_geocode.R
@@ -6,7 +6,7 @@
 #' The return type can be specified.
 #' @export
 nl_geocode <- function( location
-                      , output = c("latlon", "latlona", "more", "all")
+                      , output = c("latlon", "latlona", "rd", "rda", "all")
                       #, source = c("google", "dsk") not needed
                       , messaging = FALSE
                       #, force = ifelse(source == "dsk", FALSE, TRUE), sensor = FALSE, override_limit = FALSE
@@ -14,10 +14,12 @@ nl_geocode <- function( location
                       #, signature = ""
                       #, nameType = c("long", "short")
                       #, data
+                      , type = NULL
                       , ...
                       , verbose = !messaging
                       ){
 
-  res <- nl_free(q = lociation, ..., verbose = verbose)
+  res <- nl_free(q = location, type = type, ..., verbose = verbose)
   # TODO convert the result into a sf object.
+  res
 }
--- R/params.R
+++ R/params.R
@@ -1,6 +1,12 @@
 as_params <- function(..., .list = NULL){
   .list <- as.list(.list)
   l <- list(...)
+
+  # trick to remove NULL entries
+  for (n in names(l)){
+    l[[n]] <- l[[n]]
+  }
+
   .list[names(l)] <- l
   keys <- names(.list)
   keys <- sapply(keys, utils::URLencode, USE.NAMES = FALSE)
