22218b6f831710e40428732fcf6412283a85bae7
TWIAV <e.j.h.polle@gmail.com>
2018-09-10 17:46:35 +0200

    documentation

--- R/query.R
+++ R/query.R
@@ -1,14 +1,12 @@
 #' Dummy function only used for describing common api parameters
 #' @keywords internal
-#' @param rows maximum number of rows that will be returned, if not specified it will be 10.
-#' @param start Starting index is van het eerste resultaat dat teruggegeven wordt. Dit is
-#' zero-based. In combinatie met de rows-parameter kunnen deze services gepagineerd worden bevraagd. De default-waarde is "0"
-#' @param wt Hiermee wordt opgegeven wat het outputformaat is van de bevraging. Geldige waarden zijn "json" en "xml". De default-waarde is "json".
-#' @param indent Hiermee kan worden opgegeven of de teruggegeven JSON ingesprongen (geïndenteerd) moet worden. Geldige waarden zijn "true" en "false". De default-waarde is "true".
-#' @param fq  Hiermee kan een filter query worden opgegeven, bijv. fq=bron:BAG. Met fq=* kan de default filter query worden opgeheven. De default filter is type:(gemeente OR woonplaats OR weg OR postcode OR adres)
-#' @param lat   Hiermee kan een coördinaat (in lat/lon, ofwel WGS84) worden opgegeven. Met behulp van deze parameters worden de gevonden zoekresultaten gesorteerd op afstand van het meegegeven punt. Wanneer de locatie van de gebruiker bekend is, kan op deze manier effectiever worden gezocht.
-#' Het meegeven van een coördinaat is met name nuttig voor de suggest- en vrije geocoder-services. Hier worden meestal meerdere resultaten teruggegeven. Als decimaal scheidingsteken moet een punt worden opgegeven i.p.v. een komma.
-#' @param lon  see lat
+#' @param rows maximum number of rows to be returned. Default is 10.
+#' @param start start index.Default is 0.
+#' @param wt output format. Possible values are json and xml. Default is json.
+#' @param indent return JSON in pretty print (true) or not (false). Default is true.
+#' @param fq apply a filter to the query, e.g. fq=bron:BAG. The default filter is 'type:(gemeente OR woonplaats OR weg OR postcode OR adres)'. Use fq=* to remove the default filter.
+#' @param lat enter a decimal degree (latitude, in WGS84). Together with lon this will define a point to search from. Search results will be ordered by distance from this point. For example: 'lat=52.09&lon=5.12'
+#' @param lon enter a decimal degree (longitude, in WGS84). Together with lat this will define  point to search from. Search results will be ordered by distance from this point. For example: 'lat=52.09&lon=5.12'
 #'
 #' @seealso
 query <- function( rows    = NULL
@@ -21,25 +19,3 @@ query <- function( rows    = NULL
 ){
   stop("Dummy function, only used for documenting its arguments")
 }
-
-#' Optioneel: wt=<formaat>
-#'
-#'   Hiermee wordt opgegeven wat het outputformaat is van de bevraging. Geldige waarden zijn "json" en "xml". De default-waarde is "json".
-#'
-#' Optioneel: indent=<waarde>
-#'
-#'   Hiermee kan worden opgegeven of de teruggegeven JSON ingesprongen (geïndenteerd) moet worden. Geldige waarden zijn "true" en "false". De default-waarde is "true".
-#'
-#' Optioneel: lat=<latitude>&lon=<longitude>
-#'
-#'   Hiermee kan een coördinaat (in lat/lon, ofwel WGS84) worden opgegeven. Met behulp van deze parameters worden de gevonden zoekresultaten gesorteerd op afstand van het meegegeven punt. Wanneer de locatie van de gebruiker bekend is, kan op deze manier effectiever worden gezocht.
-#'
-#' Het meegeven van een coördinaat is met name nuttig voor de suggest- en vrije geocoder-services. Hier worden meestal meerdere resultaten teruggegeven. Als decimaal scheidingsteken moet een punt worden opgegeven i.p.v. een komma.
-#'
-#' Voorbeeld: lat=52.09&lon=5.12
-#'
-#' De resultaten worden gesorteerd op afstand van een bepaald punt in het centrum van Utrecht.
-#'
-#' Optioneel: fq=<filter query>
-#'
-#'   Hiermee kan een filter query worden opgegeven, bijv. fq=bron:BAG. Met fq=* kan de default filter query worden opgeheven. De default filter is type:(gemeente OR woonplaats OR weg OR postcode OR adres)
