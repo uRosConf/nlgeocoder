6d14ad8911109dcc198cf67e60ee797d90ee13e9
Willy <friesewoudloper@gmail.com>
2018-09-11 10:35:02 +0200

    Unit tests for nlgeocode

--- tests/testthat/test_nl_geocode.R
+++ tests/testthat/test_nl_geocode.R
@@ -1,10 +1,37 @@
 context("nl_geocode")
 
-test <- data.frame(address = c("Martinikerkhof 3, Groningen", "9712 JN 4", "Henri Faasdreef 312 Den Haag", "Hoofdstraat 1A Een"))
-
-
-
-test_that("type = adres is default", {
+test_that("Class of object returned is sf", {
   res <- nl_geocode("Martinikerkhof 3 Groningen")
-  expect_true(sum(res$response$docs$type == 'adres') == 10)
+  expect_true(class(res)[1] == "sf")
+})
+
+test_that("type = adres", {
+  res <- nl_geocode("Martinikerkhof 3 Groningen")
+  expect_true(res$type == "adres")
+})
+
+test_that("Geocode a vector of adresses", {
+  res <- nl_geocode(c("Martinikerkhof 3, Groningen", "9712 JN 4", "Henri Faasdreef 312 Den Haag", "Hoofdstraat 1A Een"))
+  expect_true(nrow(res) == 4)
+})
+
+test_that("No NULL values", {
+  res <- nl_geocode(c("Martinikerkhof 3, Groningen", "9712 JN 4", "Henri Faasdreef 312 Den Haag", "Hoofdstraat 1A Een"))
+  cols <- colnames(res)
+  for (i in seq_along(cols)){expect_true(sum(is.na(res[[cols[i]]])) == 0, label = paste0(cols[i], " does not contain NULL values"))}
+})
+
+test_that("Location returned is in WGS84", {
+  res <- nl_geocode("Martinikerkhof 3 Groningen")
+  expect_true(class(res$centroide_ll)[1] == "sfc_POINT")
+})
+
+test_that("Location returned is in RD_New", {
+  res <- nl_geocode("Martinikerkhof 3 Groningen", output = "rd")
+  expect_true(class(res$centroide_rd)[1] == "sfc_POINT")
+})
+
+test_that("Dataframe returned (not an sf object)", {
+  res <- nl_geocode("Martinikerkhof 3 Groningen", output = "data.frame")
+  expect_true(class(res) == "data.frame")
 })
