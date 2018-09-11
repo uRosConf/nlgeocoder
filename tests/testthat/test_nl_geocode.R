77ab7e8d0d358b25dca1f692c2c872113b421f8d
Willy <friesewoudloper@gmail.com>
2018-09-11 08:52:50 +0200

    Updated files

--- tests/testthat/test_nl_geocode.R
+++ tests/testthat/test_nl_geocode.R
@@ -1,10 +1,10 @@
 context("nl_geocode")
 
-test <- data.frame(address = c("Martinikerkhof 3, Groningen", "9712 JN 4", "Henri Faasdreef 312 Den Haag"))
+test <- data.frame(address = c("Martinikerkhof 3, Groningen", "9712 JN 4", "Henri Faasdreef 312 Den Haag", "Hoofdstraat 1A Een"))
 
-# Iets met huisnummer toevoeging
+
 
 test_that("type = adres is default", {
-  res <- nl_geocode(test$address)
-  expect_true(sum(res$type == 'adres') == 3)
+  res <- nl_geocode("Martinikerkhof 3 Groningen")
+  expect_true(sum(res$response$docs$type == 'adres') == 10)
 })
