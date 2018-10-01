27525c948a5cc998d83ca3246e452832ffe11a6b
Edwin de Jonge <edwindjonge@gmail.com>
2018-10-01 14:24:50 +0200

    commit

--- R/package.R
+++ R/package.R
@@ -20,6 +20,7 @@ API <- "https://geodata.nationaalgeoregister.nl/locatieserver/v3"
 
 
 #' Several Points of Interest (POI) that are included for test purposes.
+#'
 #' \itemize{
 #'   \item \code{POI}, points of interest
 #'   \item \code{Address}, Uncleaned address information.
